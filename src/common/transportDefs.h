7e3d6f3689dd55b9e163e149ab9ecde12ea58881
Venca_X <Venca_X@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2007-12-02 20:08:16 +0000

    Data storage project founded.

---
+++ src/common/transportDefs.h
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////
+// Contains some common declarations for communicaton
+// between client & server
+//////////////////////////////////////////////////
+
+// messages identifications
+typedef enum eMessageType {
+	MESS_DATA_REQUEST,		// client wants some data
+	MESS_DATA_SEND,			// sevrver sends requested data
+	MESS_DATA_STORE,		// client wants to sotre some data
+	MESS_PING,				// ping to see if server is alive
+	/* ... */
+} MessageType;
+
+
+// distinguish data requests
+typedef enum eRequestType {
+	REQTYPE_BITMAP,
+	REQTYPE_TENSORMAP,
+	/* ... */
+} RequestType;
