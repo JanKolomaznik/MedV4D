79c4df56a2f53934e8dc4031270c85bd9445953e
johny <johny@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2007-12-18 23:20:23 +0000

    Changing vtk integration base class from vtkDataSet to vtkGeneralDataSet. vtkDataObject isn't needed any more.

--- src/include/m4dAbstractFilter.h
+++ src/include/m4dAbstractFilter.h
@@ -1,9 +1,9 @@
 #ifndef __M4D_ABSTRACT_FILTER_H_
 #define __M4D_ABSTRACT_FILTER_H_
 
-#include <vtkAlgorithm.h>
+#include <vtkGenericDataSetAlgorithm.h>
 
-class m4dAbstractFilter: public vtkAlgorithm
+class m4dAbstractFilter: public vtkGenericDataSetAlgorithm
 {
 public:
 
@@ -12,88 +12,91 @@ protected:
 private:
 
 public: /*inherited from VTK*/
-	virtual const char*	GetClassName ()
-	virtual int		IsA (const char *type);
-	void			PrintSelf (ostream &os, vtkIndent indent);
-	int			HasExecutive ();
-	vtkExecutive*		GetExecutive ();
-	virtual void		SetExecutive (vtkExecutive *executive);
-	virtual int		ModifyRequest (vtkInformation *request, int when);
-	vtkInformation*		GetInputPortInformation (int port);
-	vtkInformation*		GetOutputPortInformation (int port);
-	int			GetNumberOfInputPorts ();
-	int			GetNumberOfOutputPorts ();
-	void			UpdateProgress (double amount);
-	vtkInformation*		GetInputArrayInformation (int idx);
-	void			RemoveAllInputs ();
-	vtkDataObject*		GetOutputDataObject (int port);
-	virtual void		RemoveInputConnection (int port, vtkAlgorithmOutput *input);
-	int			GetNumberOfInputConnections (int port);
-	int			GetTotalNumberOfInputConnections ();
-	vtkAlgorithmOutput*	GetInputConnection (int port, int index);
-	virtual void		Update ();
-	virtual void		UpdateInformation ();
-	virtual void		UpdateWholeExtent ();
-	void			ConvertTotalInputToPortConnection (int ind, int &port, int &conn);
-	virtual int		ProcessRequest (
-					vtkInformation 		*request, 
-					vtkInformationVector 	**inInfo, 
-					vtkInformationVector 	*outInfo
-				);
-	virtual int		ComputePipelineMTime (
-					vtkInformation 		*request, 
-					vtkInformationVector 	**inInfoVec, 
-					vtkInformationVector 	*outInfoVec, 
-					int 			requestFromOutputPort, 
-					unsigned long 		*mtime
-				);
-	virtual vtkInformation *	GetInformation ();
-	virtual void		SetInformation (vtkInformation *);
-	virtual void		Register (vtkObjectBase *o);
-	virtual void		UnRegister (vtkObjectBase *o);
-	virtual void		SetAbortExecute (int);
-	virtual int		GetAbortExecute ();
-	virtual void		AbortExecuteOn ();
-	virtual void		AbortExecuteOff ();
-	virtual void		SetProgress (double);
-	virtual double		GetProgress ();
-	virtual void		SetProgressText (const char *);
-	virtual char*		GetProgressText ();
-	virtual unsigned long	GetErrorCode ();
-	void			SetInputArrayToProcess (
-					int 		idx, 
-					int 		port, 
-					int 		connection, 
-					int 		fieldAssociation, 
-					const char 	*name
-				);
-	void			SetInputArrayToProcess (
-					int 		idx, 
-					int 		port, 
-					int 		connection, 
-					int 		fieldAssociation, 
-					int 		fieldAttributeType
-				);
-	void			SetInputArrayToProcess (int idx, vtkInformation *info);
-	void			SetInputArrayToProcess (
-					int 		idx, 
-					int 		port, 
-					int 		connection, 
-					const char 	*fieldAssociation, 
-					const char 	*attributeTypeorName
-				);
-	virtual void		SetInputConnection (int port, vtkAlgorithmOutput *input);
-	virtual void		SetInputConnection (vtkAlgorithmOutput *input);
-	virtual void		AddInputConnection (int port, vtkAlgorithmOutput *input);
-	virtual void		AddInputConnection (vtkAlgorithmOutput *input);
-	vtkAlgorithmOutput*	GetOutputPort (int index);
-	vtkAlgorithmOutput*	GetOutputPort ();
-	virtual void		SetReleaseDataFlag (int);
-	virtual int		GetReleaseDataFlag ();
-	void			ReleaseDataFlagOn ();
-	void			ReleaseDataFlagOff ();
-	int			UpdateExtentIsEmpty (vtkDataObject *output);
-	int			UpdateExtentIsEmpty (vtkInformation *pinfo, int extentType);
+	vtkTypeRevisionMacro(vtkGenericDataSetAlgorithm,vtkAlgorithm);
+	void PrintSelf(ostream& os, vtkIndent indent);
+
+	// Description:
+	// Get the output data object for a port on this algorithm.
+	vtkGenericDataSet* GetOutput();
+	vtkGenericDataSet* GetOutput(int);
+	virtual void SetOutput(vtkDataObject* d);
+
+	// Description:
+	// see vtkAlgorithm for details
+	virtual int ProcessRequest(vtkInformation*,
+			     vtkInformationVector**,
+			     vtkInformationVector*);
+
+	// this method is not recommended for use, but lots of old style filters
+	// use it
+	vtkDataObject* GetInput();
+	vtkDataObject *GetInput(int port);
+	vtkGenericDataSet *GetGenericDataSetInput(int port);
+
+	// Description:
+	// Set an input of this algorithm. You should not override these
+	// methods because they are not the only way to connect a pipeline.
+	// Note that these methods support old-style pipeline connections.
+	// When writing new code you should use the more general
+	// vtkAlgorithm::SetInputConnection().  These methods transform the
+	// input index to the input port index, not an index of a connection
+	// within a single port.
+	void SetInput(vtkDataObject *);
+	void SetInput(int, vtkDataObject*);
+
+	// Description:
+	// Add an input of this algorithm.  Note that these methods support
+	// old-style pipeline connections.  When writing new code you should
+	// use the more general vtkAlgorithm::AddInputConnection().  See
+	// SetInput() for details.
+	void AddInput(vtkDataObject *);
+	void AddInput(int, vtkDataObject*);
+
+protected:
+	m4dAbstractFilter();
+	~m4dAbstractFilter();
+
+	// convenience method
+	virtual int RequestInformation(vtkInformation* request,
+				 vtkInformationVector** inputVector,
+				 vtkInformationVector* outputVector);
+
+	// Description:
+	// This is called by the superclass.
+	// This is the method you should override.
+	// See ProcessRequest for details about arguments and return value.
+	virtual int RequestData(vtkInformation* request,
+			  vtkInformationVector** inputVector,
+			  vtkInformationVector* outputVector);
+
+	// Description:
+	// This is called by the superclass.
+	// This is the method you should override.
+	// See ProcessRequest for details about arguments and return value.
+	virtual int RequestDataObject(vtkInformation* request,
+				vtkInformationVector** inputVector,
+				vtkInformationVector* outputVector)=0;
+
+	// Description:
+	// This is called by the superclass.
+	// This is the method you should override.
+	// See ProcessRequest for details about arguments and return value.
+	virtual int RequestUpdateExtent(vtkInformation*,
+				  vtkInformationVector**,
+				  vtkInformationVector*);
+
+	// Description:
+	// This method is the old style execute method
+	virtual void ExecuteData(vtkDataObject *output);
+	virtual void Execute();
+
+	// see algorithm for more info
+	virtual int FillOutputPortInformation(int port, vtkInformation* info);
+	virtual int FillInputPortInformation(int port, vtkInformation* info);
+
+private:
+	m4dAbstractFilter(const m4dAbstractFilte&);  // Not implemented.
+	void operator=(const m4dAbstractFilter&);  // Not implemented.
 };
 
 #endif /*__M4D_ABSTRACT_FILTER_H_*/
--- src/include/m4dDataSet.h
+++ src/include/m4dDataSet.h
@@ -1,12 +1,12 @@
 #ifndef __M4D_DATA_SET_H_
 #define __M4D_DATA_SET_H_
 
-#include <vtkDataSet.h>
+#include <vtkGenericDataSet.h>
 
 class m4dCompatibilityAgregation;
 
 
-class m4dDataSet: public vtkDataSet
+class m4dDataSet: public vtkGenericDataSet
 {
 public:
 
@@ -15,78 +15,175 @@ protected:
 private:
 
 public: /*inherited from VTK*/
-	virtual const char*	GetClassName ();
-	virtual int		IsA (const char *type);
-	void 			PrintSelf (ostream &os, vtkIndent indent);
-	virtual void 		CopyStructure (vtkDataSet *ds)=0;
-	virtual vtkIdType 	GetNumberOfPoints ()=0;
-	virtual vtkIdType 	GetNumberOfCells ()=0;
-	virtual double*		GetPoint (vtkIdType ptId)=0;
-	virtual void		GetPoint (vtkIdType id, double x[3]);
-	virtual vtkCell*	GetCell (vtkIdType cellId)=0;
-	virtual void		GetCell (vtkIdType cellId, vtkGenericCell *cell)=0;
-	virtual void		GetCellBounds (vtkIdType cellId, double bounds[6]);
-	virtual int		GetCellType (vtkIdType cellId)=0;
-	virtual void		GetCellTypes (vtkCellTypes *types);
-	virtual void		GetCellPoints (vtkIdType cellId, vtkIdList *ptIds)=0;
-	virtual void		GetPointCells (vtkIdType ptId, vtkIdList *cellIds)=0;
-	unsigned long int 	GetMTime ();
+	void PrintSelf(ostream& os, vtkIndent indent);
 
-/*PROBLEM*/	vtkCellData*		GetCellData ();
-/*PROBLEM*/	vtkPointData*		GetPointData ();
+	// Description:
+	// Return the number of points composing the dataset. See NewPointIterator()
+	// for more details.
+	// \post positive_result: result>=0
+	virtual vtkIdType GetNumberOfPoints() = 0;
 
-	virtual void		Squeeze ();
-	virtual void		ComputeBounds ();
-	double*		 	GetBounds ();
-	void 			GetBounds (double bounds[6]);
-	double * 		GetCenter ();
-	void 			GetCenter (double center[3]);
-	double 			GetLength ();
-	void 			Initialize ();
-	virtual void 		GetScalarRange (double range[2]);
-	double * 		GetScalarRange ();
-	virtual int 		GetMaxCellSize ()=0;
-	unsigned long 		GetActualMemorySize ();
-	int 			CheckAttributes ();
-	virtual void 		GenerateGhostLevelArray ();
-	virtual void 		GetCellNeighbors (
-					vtkIdType 	cellId, 
-					vtkIdList	*ptIds, 
-					vtkIdList	*cellIds
-				);
-	vtkIdType 		FindPoint (double x, double y, double z);
-	virtual vtkIdType 	FindPoint (double x[3])=0;
-	virtual vtkIdType 	FindCell (
-					double 		x[3], 
-					vtkCell 	*cell, 
-					vtkIdType 	cellId, 
-					double		tol2, 
-					int		&subId, 
-					double		pcoords[3], 
-					double 		*weights
-				)=0;
-	virtual vtkIdType 	FindCell (
-					double 	x[3], 
-					vtkCell 	*cell, 
-					vtkGenericCell	*gencell, 
-					vtkIdType 	cellId, 
-					double 		tol2, 
-					int 		&subId, 
-					double		pcoords[3], 
-					double 		*weights
-				)=0;
-	virtual vtkCell * 	FindAndGetCell (
-					double 		x[3], 
-					vtkCell 	*cell, 
-					vtkIdType 	cellId, 
-					double 		tol2, 
-					int 		&subId, 
-					double 		pcoords[3], 
-					double 		*weights
-				);
-	int 			GetDataObjectType ();
-	void 			ShallowCopy (vtkDataObject *src);
-	void 			DeepCopy (vtkDataObject *src);
+	// Description:
+	// Return the number of cells that explicitly define the dataset. See 
+	// NewCellIterator() for more details.
+	// \pre valid_dim_range: (dim>=-1) && (dim<=3)
+	// \post positive_result: result>=0
+	virtual vtkIdType GetNumberOfCells(int dim=-1) = 0;
+
+	// Description:
+	// Return -1 if the dataset is explicitly defined by cells of varying
+	// dimensions or if there are no cells. If the dataset is explicitly
+	// defined by cells of a unique dimension, return this dimension.
+	// \post valid_range: (result>=-1) && (result<=3)
+	virtual int GetCellDimension() = 0;
+
+	// Description:
+	// Get a list of types of cells in a dataset. The list consists of an array
+	// of types (not necessarily in any order), with a single entry per type.
+	// For example a dataset 5 triangles, 3 lines, and 100 hexahedra would
+	// result a list of three entries, corresponding to the types VTK_TRIANGLE,
+	// VTK_LINE, and VTK_HEXAHEDRON.
+	// THIS METHOD IS THREAD SAFE IF FIRST CALLED FROM A SINGLE THREAD AND
+	// THE DATASET IS NOT MODIFIED
+	// \pre types_exist: types!=0
+	virtual void GetCellTypes(vtkCellTypes *types);
+
+	// Description:
+	// Return an iterator to traverse cells of dimension `dim' (or all
+	// dimensions if -1) that explicitly define the dataset. For instance, it
+	// will return only tetrahedra if the mesh is defined by tetrahedra. If the
+	// mesh is composed of two parts, one with tetrahedra and another part with
+	// triangles, it will return both, but will not return the boundary edges
+	// and vertices of these cells. The user is responsible for deleting the
+	// iterator.
+	// \pre valid_dim_range: (dim>=-1) && (dim<=3)
+	// \post result_exists: result!=0
+	virtual vtkGenericCellIterator *NewCellIterator(int dim=-1) = 0;
+
+	// Description:
+	// Return an iterator to traverse cell boundaries of dimension `dim' (or
+	// all dimensions if -1) of the dataset.  If `exteriorOnly' is true, only
+	// the exterior cell boundaries of the dataset will be returned, otherwise
+	// it will return exterior and interior cell boundaries. The user is
+	// responsible for deleting the iterator.
+	// \pre valid_dim_range: (dim>=-1) && (dim<=2)
+	// \post result_exists: result!=0
+	virtual vtkGenericCellIterator *NewBoundaryIterator(int dim=-1,
+						      int exteriorOnly=0) = 0;
+
+	// Description:
+	// Return an iterator to traverse the points composing the dataset; they
+	// can be points that define a cell or isolated. The user is responsible
+	// for deleting the iterator.
+	// \post result_exists: result!=0
+	virtual vtkGenericPointIterator *NewPointIterator()=0;
+
+	// Description:
+	// Locate the closest cell to position `x' (global coordinates) with
+	// respect to a tolerance squared `tol2' and an initial guess `cell' (if
+	// valid). The result consists in the `cell', the `subId' of the sub-cell
+	// (0 if primary cell), the parametric coordinates `pcoord' of the
+	// position. It returns whether the position is inside the cell or
+	// not (boolean). Tolerance is used to control how close the point is to be
+	// considered "in" the cell.
+	// THIS METHOD IS NOT THREAD SAFE.
+	// \pre not_empty: GetNumberOfCells()>0
+	// \pre cell_exists: cell!=0
+	// \pre positive_tolerance: tol2>0
+	virtual int FindCell(double x[3],
+		       vtkGenericCellIterator* &cell,
+		       double tol2,
+		       int &subId,
+		       double pcoords[3]) = 0;
+
+	// Description:
+	// Locate the closest point `p' to position `x' (global coordinates).
+	// \pre not_empty: GetNumberOfPoints()>0
+	// \pre p_exists: p!=0
+	virtual void FindPoint(double x[3],
+			 vtkGenericPointIterator *p)=0;
+
+	// Description:
+	// Datasets are composite objects and need to check each part for their
+	// modified time.
+	virtual unsigned long int GetMTime();
+
+	// Description:
+	// Compute the geometry bounding box.
+	virtual void ComputeBounds()=0;
+
+	// Description:
+	// Return a pointer to the geometry bounding box in the form
+	// (xmin,xmax, ymin,ymax, zmin,zmax).
+	// The return value is VOLATILE.
+	// \post result_exists: result!=0
+	virtual double *GetBounds();
+
+	// Description:
+	// Return the geometry bounding box in global coordinates in
+	// the form (xmin,xmax, ymin,ymax, zmin,zmax) in the `bounds' array.
+	virtual void GetBounds(double bounds[6]);
+
+	// Description:
+	// Get the center of the bounding box in global coordinates.
+	// The return value is VOLATILE.
+	// \post result_exists: result!=0
+	virtual double *GetCenter();
+
+	// Description:
+	// Get the center of the bounding box in global coordinates.
+	virtual void GetCenter(double center[3]);
+
+	// Description:
+	// Return the length of the diagonal of the bounding box.
+	// \post positive_result: result>=0
+	virtual double GetLength();
+
+	// Description:
+	// Get the collection of attributes associated with this dataset.
+	vtkGetObjectMacro(Attributes, vtkGenericAttributeCollection);
+
+	// Description:
+	// Set/Get a cell tessellator if cells must be tessellated during
+	// processing.
+	// \pre tessellator_exists: tessellator!=0
+	virtual void SetTessellator(vtkGenericCellTessellator *tessellator);
+	vtkGetObjectMacro(Tessellator,vtkGenericCellTessellator);
+
+	// Description:
+	// Actual size of the data in kilobytes; only valid after the pipeline has
+	// updated. It is guaranteed to be greater than or equal to the memory
+	// required to represent the data.
+	virtual unsigned long GetActualMemorySize();
+
+	// Description:
+	// Return the type of data object.
+	int GetDataObjectType();
+
+	// Description:
+	// Estimated size needed after tessellation (or special operation)
+	virtual vtkIdType GetEstimatedSize() = 0;
+
+	//BTX
+	// Description:
+	// Retrieve an instance of this class from an information object.
+	static vtkGenericDataSet* GetData(vtkInformation* info);
+	static vtkGenericDataSet* GetData(vtkInformationVector* v, int i=0);
+	//ETX
+
+protected:
+	// Description:
+	// Constructor with uninitialized bounds (1,-1, 1,-1, 1,-1),
+	// empty attribute collection and default tessellator.
+	m4dDataSet();
+
+	virtual ~m4dDataSet();
+
+	vtkTimeStamp ComputeTime; // Time at which bounds, center, etc. computed
+
+private:
+	m4dDataSet(const m4dDataSet&);  // Not implemented.
+	void operator=(const m4dDataSet&);    // Not implemented.
 };
 
 
