d3d1d4ceca50620a78f95fff9e550476e49ff93f
Venca_X <Venca_X@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2008-04-08 12:23:13 +0000

    sample of usage dicomClient library project

---
+++ src/project/dicomCLientSample/main.cpp
@@ -0,0 +1,61 @@
+
+#include "Debug.h"
+#include "Log.h"
+#include "M4DCommon.h"
+#include "dicomConn/M4DDICOMServiceProvider.h"
+
+using namespace std;
+
+int
+main( void)
+{
+	M4DDcmProvider::ResultSet result;
+	M4DDcmProvider::StudyInfo studyInfo;
+	M4DDcmProvider::DicomObjSet obj;	
+
+	string patientName = "";
+	string patientID = "";
+	string modality = "";
+	string dateFrom = "";
+
+	try {
+		// provider object
+		M4DDcmProvider provider;
+
+		// find some patient & studies info
+		provider.Find( 
+			result, patientName, patientID, modality, dateFrom);
+
+		M4DDcmProvider::TableRow *row = &result[0];
+
+		// find some info about selected study
+		provider.WholeFindStudyInfo( row->patentID, row->studyID, studyInfo);
+
+		// now get image
+		provider.GetImageSet( row->patentID, row->studyID,
+			studyInfo.begin()->first, obj);
+
+		int i=0;
+		char fileName[32];
+		string pok;
+		for( M4DDcmProvider::DicomObjSet::iterator it = obj.begin();
+			it != obj.end();
+			it++)
+		{
+			//pok = it->GetPatientName();
+			sprintf( fileName, "C:\\pok%d.dcm", i++);
+			it->Save( fileName );
+		}
+
+		//M4DDcmProvider::DicomObj obj;
+
+
+
+	} catch( bad_exception *e) {
+		string what = e->what();
+		delete e;
+		return -1;
+	}
+
+    return 0;
+}
