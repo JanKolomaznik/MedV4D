8e975c3a9e9892c8f0a0a8bdf4800fba4a922e79
ulman <ulman@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2008-04-10 10:33:06 +0000

    Adding pilot application (with project file) for testing widgets and other components of the project.

---
+++ src/GUI/m4dPilot/main.cpp
@@ -0,0 +1,21 @@
+#include <QtGui/QApplication>
+
+#include "m4dGUIMainWindow.h"
+
+#include "../../include/M4DCommon.h"
+
+// not a final solution
+#ifdef OS_WIN
+  #include "projectConfig.h"
+#endif
+
+int main ( int argc, char *argv[] )
+{
+  QApplication app( argc, argv );
+  app.setQuitOnLastWindowClosed( true );
+
+  m4dGUIMainWindow mainWindow;
+  mainWindow.show();
+
+  return app.exec();
+}
