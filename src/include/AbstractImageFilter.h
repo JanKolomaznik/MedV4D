6dc1e3883e0a9f95ae47d93ce5747f1185968809
johny <johny@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2008-04-21 14:33:46 +0000

    Minor reorganisation.

---
+++ src/include/AbstractImageFilter.h
@@ -0,0 +1,42 @@
+#ifndef _ABSTRACT_IMAGE_FILTER_H
+#define _ABSTRACT_IMAGE_FILTER_H
+
+#include "ExceptionBase.h"
+
+#include "ImageDataTemplate.h"
+#include "ImageFactory.h"
+
+namespace M4D
+{
+
+namespace Images
+{
+
+/**
+ * Ancestor of all image filters.
+ **/
+class BaseFilter
+{
+
+};
+
+
+/**
+ * Ancestor of filters with single input and single output.
+ **/
+template< typename InputElementType, typename OutputElementType >
+class AbstractImageFilter: public BaseFilter
+{
+public:
+
+protected:
+
+private:
+
+};
+
+} /*namespace Images*/
+} /*namespace M4D*/
+
+
+#endif /*_ABSTRACT_IMAGE_FILTER_H*/
