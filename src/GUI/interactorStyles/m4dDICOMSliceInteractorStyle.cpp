6db677401da1c294956ce09e6efe40b1502bde94
szabig <szabig@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2008-06-09 15:33:31 +0000

    Added InteractorStyle that displays DICOM image slices one-by-one as the mouse wheel is rotated. A special reader is used that inherits from vtkDICOMImageReader.

---
+++ src/GUI/interactorStyles/m4dDICOMSliceInteractorStyle.cpp
@@ -0,0 +1,62 @@
+#include "m4dDICOMSliceInteractorStyle.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderWindow.h"
+#include "string.h"
+
+m4dDICOMSliceInteractorStyle* m4dDICOMSliceInteractorStyle::New()
+{
+  return new m4dDICOMSliceInteractorStyle();
+}
+
+void m4dDICOMSliceInteractorStyle::OnMouseWheelForward()
+{
+  currentSlice++;
+  vtkRenderer *ren = sliceReader->getSlice( currentDirectory, currentSlice );
+  if ( ren )
+  {
+    if ( currentRenderer ) GetInteractor()->GetRenderWindow()->RemoveRenderer(currentRenderer);
+    currentRenderer = ren;
+    GetInteractor()->GetRenderWindow()->AddRenderer(currentRenderer);
+    GetInteractor()->GetRenderWindow()->Render();
+  }
+}
+
+void m4dDICOMSliceInteractorStyle::OnMouseWheelBackward()
+{
+  currentSlice--;
+  vtkRenderer *ren = sliceReader->getSlice( currentDirectory, currentSlice );
+  if ( ren )
+  {
+    if ( currentRenderer ) GetInteractor()->GetRenderWindow()->RemoveRenderer(currentRenderer);
+    currentRenderer = ren;
+    GetInteractor()->GetRenderWindow()->AddRenderer(currentRenderer);
+    GetInteractor()->GetRenderWindow()->Render();
+  }
+}
+
+bool m4dDICOMSliceInteractorStyle::initRenderer( const char* dir )
+{
+  vtkRenderer *ren = sliceReader->getSlice( dir, currentSlice );
+  if ( ren )
+  {
+    currentDirectory = new char[ strlen(dir) + 1 ];
+    strncpy(currentDirectory,dir,strlen(dir));
+    if ( currentRenderer ) GetInteractor()->GetRenderWindow()->RemoveRenderer(currentRenderer);
+    currentRenderer = ren;
+    GetInteractor()->GetRenderWindow()->AddRenderer(currentRenderer);
+    GetInteractor()->GetRenderWindow()->Render();
+    return true;
+  }
+  return false;
+}
+
+m4dDICOMSliceInteractorStyle::m4dDICOMSliceInteractorStyle()
+{
+  currentSlice = 0;
+  sliceReader = m4dDICOMSliceReader::New();
+  currentRenderer = 0;
+}
+
+m4dDICOMSliceInteractorStyle::~m4dDICOMSliceInteractorStyle()
+{
+}
---
+++ src/include/GUI/m4dDICOMSliceReader.h
@@ -0,0 +1,30 @@
+#ifndef __m4dDICOMSliceReader_h
+#define __m4dDICOMSliceReader_h
+
+#include "vtkDICOMImageReader.h"
+#include "vtkRenderer.h"
+
+class m4dDICOMSliceReader : public vtkDICOMImageReader
+{
+
+  public:
+
+    // Create a new instance.
+    static m4dDICOMSliceReader* New();
+
+    // Get the slice with number "slice" from the images located in directory "dir".
+    // If slice number is too small, it will be modified to 0; if it is too large,
+    // it will be modified to the number of the last image slice.
+    vtkRenderer* getSlice( const char* dir, int& slice );
+
+  protected:
+    m4dDICOMSliceReader();
+    ~m4dDICOMSliceReader();
+
+  private:
+    m4dDICOMSliceReader(const m4dDICOMSliceReader&); // Not implemented.
+    void operator=(const m4dDICOMSliceReader&); // Not implemented.
+
+};
+
+#endif
