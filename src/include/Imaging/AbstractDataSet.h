2a90674763ed44a0fe109afc05ffdaa63b8db139
johny <johny@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2008-07-09 08:51:52 +0000

    Adding cast methods to AbstractDataSet.

--- src/include/Imaging/AbstractDataSet.h
+++ src/include/Imaging/AbstractDataSet.h
@@ -21,6 +21,33 @@ public:
 	const M4D::Common::TimeStamp&
 	GetStructureTimestamp()const
 		{ return _structureTimestamp; }
+
+	template< typename DatasetType >
+	static AbstractDataSet &
+	CastDataSet(  DatasetType & dataset )
+	{
+		//TODO - handle exception well
+		return dynamic_cast< AbstractDataSet & >( dataset );
+	}
+
+	template< typename DatasetType >
+	static const AbstractDataSet &
+	CastDataSet( const DatasetType & dataset )
+	{
+		//TODO - handle exception well
+		return dynamic_cast< const AbstractDataSet & >( dataset );
+	}
+
+	template< typename DatasetType >
+	static typename AbstractDataSet::ADataSetPtr
+	CastDataSet( boost::shared_ptr< DatasetType > & dataset )
+	{
+		if( dynamic_cast< DatasetType * >( dataset.get() ) == NULL ) {
+		//TODO throw exception
+		}
+
+		return boost::static_pointer_cast< AbstractDataSet >( dataset );
+	}
 protected:
 	/**
 	 * Increase structure timestamp - only helper function for successors.
