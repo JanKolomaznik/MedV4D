3c22fd4587d463e4021ac0f18110fe15460de678
johny <johny@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2008-08-12 15:13:52 +0000

    Skeleton of serializers.

--- src/include/cellBE/GeneralFilterSerializer.h
+++ src/include/cellBE/GeneralFilterSerializer.h
@@ -29,12 +29,45 @@ public:
    *  that represents a filter. Returned Serializer is later used for
    *  serializing the properties of the filter it represents.
    */
+  template< typename FilterProperties >
   static AbstractFilterSerializer *
-  GetFilterSerializer(
-    M4D::Imaging::AbstractPipeFilter::Properties *props);
+  GetFilterSerializer( FilterProperties *props );
 
 };
 
+
+template< typename FilterProperties >
+AbstractFilterSerializer *
+GeneralFilterSerializer::GetFilterSerializer( FilterProperties *props )
+{
+	return new FilterSerializer< FilterProperties >( props );
+}
+
+
+//****************************************************************
+
+//Empty declaration - we allow only partial specializations
+template< typename FilterProperties >
+class FilterSerializer;
+
+template< typename InputImageType >
+class FilterSerializer< M4D::Imaging::ThresholdingFilter< InputImageType >::Properties > 
+	: public AbstractFilterSerializer
+{
+public:
+	typedef typename M4D::Imaging::ThresholdingFilter< InputImageType >::Properties Properties;
+	
+	FilterSerializer( Properties * props) 
+		: AbstractFilterSerializer( GetFilterID( *props ) ), _properties( props ) {}
+
+	~FilterSerializer() { delete _properties; }
+
+	void 
+	SerializeProperties( M4D::CellBE::NetStream &s);
+	
+protected:
+	Properties	_properties;
+};
 
 
 }
