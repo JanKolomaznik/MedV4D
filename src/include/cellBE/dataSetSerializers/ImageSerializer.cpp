e94a82ff55912da9399272cb0a263118cda89f27
johny <johny@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2008-08-20 18:28:17 +0000

    Pilot version

--- src/include/cellBE/dataSetSerializers/ImageSerializer.cpp
+++ src/include/cellBE/dataSetSerializers/ImageSerializer.cpp
@@ -5,28 +5,72 @@
 using namespace M4D::CellBE;
 using namespace M4D::Imaging;
 
+
+template< typename ElementType >
+void
+SerializeImage( Image< ElementType, 2 > &image )
+{
+	uint32 width;
+	uint32 height;
+	int32 xStride;
+	int32 yStride;
+	ElementType *pointer = in.GetPointer( width, height, xStride, yStride );
+	for( uint32 j = 0; j < height; ++j ) {
+		ElementType *tmpPointer = pointer + j*yStride;
+
+		for( uint32 i = 0; i < width; ++i ) {
+			//tady zapsani jednoho elementu
+			// << *tmpPointer;
+
+			tmpPointer += xStride;
+		}
+	}
+
+}
+
+
+template< typename ElementType >
+void
+SerializeImage( Image< ElementType, 3 > &image )
+{
+	uint32 width;
+	uint32 height;
+	uint32 depth;
+	int32 xStride;
+	int32 yStride;
+	int32 zStride;
+	ElementType *pointer = in.GetPointer( width, height, depth, xStride, yStride, zStride );
+
+	for( uint32 k = 0; k < depth; ++k ) {
+		for( uint32 j = 0; j < height; ++j ) {
+			ElementType *tmpPointer = pointer + j*yStride + k*zStride;
+
+			for( uint32 i = 0; i < width; ++i ) {
+				//tady zapsani jednoho elementu
+				// << *tmpPointer;
+
+				tmpPointer += xStride;
+			}
+		}
+	}
+}
 ///////////////////////////////////////////////////////////////////////////////
 
 void
 ImageSerializer::SerializeProperties(M4D::CellBE::NetStream &s)
 {
-  AbstractImage *im = (AbstractImage *) m_dataSet; // cast to sucessor
+	AbstractImage *im = (AbstractImage *) m_dataSet; // cast to sucessor
 
-  // serialize common properties
-  s << (uint8) im->GetDimension() << (uint8) im->GetElementTypeID();
+	// serialize common properties
+	s << (uint16) im->GetDimension() << (uint16) im->GetElementTypeID();
 
-  // now switch according dimensionality
-  switch( im->GetDimension() )
-  {
-  case 2:
-    break;
+	for( unsigned i = 0; i < im->GetDimension(); ++i ) {
+		const DimensionExtents &dimExtents = im->GetDimensionExtents( i );
 
-  case 3:
-    break;
-
-  case 4:
-    break;
-  }
+		s << (int32)dimExtents.minimum;
+		s << (int32)dimExtents.maximum;
+		s << (float32)dimExtents.elementExtent;
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -35,23 +79,28 @@ M4D::Imaging::AbstractDataSet *
 ImageSerializer::DeSerializeProperties(M4D::CellBE::NetStream &s)
 {
 
-  uint8 dim, elemType;
-  s >> dim >> elemType;   // get common properties
+	uint16 dim, elemType;
+	s >> dim >> elemType;   // get common properties
+	
+	int32 *minimums = new int32[ dim ];
+	int32 *maximums = new int32[ dim ];
+	float32 *elExtents = new float32[ dim ];
 
-  // construct new Image instance according that common props
-  switch( dim)
-  {
-  case 2:
-    break;
+	for( unsigned i = 0; i < dim; ++i ) {
+		const DimensionExtents &dimExtents = im->GetDimensionExtents( i );
 
-  case 3:
-    break;
+		s >> minimums[ i ];
+		s >> maximums[ i ];
+		s >> elExtents[ i ];
+	}
 
-  case 4:
-    break;
-  }
+	//TODO create dataset
 
-  return m_dataSet; // TODO initialize
+	delete [] minimums;
+	delete [] maximums;
+	delete [] elExtents;
+
+	return m_dataSet; // TODO initialize
 
 }
 
@@ -75,6 +124,14 @@ ImageSerializer::OnDataSetEndRead( void)
 void
 ImageSerializer::Serialize( M4D::CellBE::iPublicJob *job)
 {
+	AbstractImage *im = (AbstractImage *) m_dataSet;
+
+	NUMERIC_TYPE_TEMPLATE_SWITCH_MACRO( image->GetElementTypeID(), 
+		DIMENSION_TEMPLATE_SWITCH_MACRO( 
+			image->GetDimension(), 
+			SerializeImage( (const Image< TTYPE, DIM >&) *image ) 
+			)
+		);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
