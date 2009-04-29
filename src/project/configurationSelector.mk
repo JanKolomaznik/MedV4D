e83fc12b16212e3ddc94d0a96f130859941439d6
Venca_X <Venca_X@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2009-04-29 19:05:14 +0000

    build configs

--- src/project/configurationSelector.mk
+++ src/project/configurationSelector.mk
@@ -1,14 +1,33 @@
+ifndef CONF
+CONF = Debug
+endif
 
 CXXFLAGS= -Wall -Wno-deprecated
 # verbose compiler
 #CXXFLAGS += -v
-#debug opts
-CXXFLAGS += -g -pg -DDEBUG_LEVEL=10 -DDEBUG_ADITIONAL_INFO
+
+####################### CONFs ############################
+ifeq "$(CONF)" "Debug"
+CXXFLAGS = -g -DDEBUG_LEVEL=10 -DDEBUG_ADITIONAL_INFO
+endif
+
+ifeq "$(CONF)" "Release"
+CXXFLAGS = -DDEBUG_LEVEL=1
+endif
+
+ifeq "$(CONF)" "Profile"
+CXXFLAGS = -g -pg -DDEBUG_LEVEL=10 -DDEBUG_ADITIONAL_INFO
+endif
+##########################################################
+
 
 # linker flags
 #-Wl,-q
 LD_PROFILE_OPTS= -pg
+
+ifeq "$(CONF)" "profile"
 LDFLAGS= $(LD_PROFILE_OPTS)
+endif
 
 #Dependecy file creation parameters
 DEP_FILE=	$(TMP_DIR)/depend
