1654529a9c98c92719fba5a515972ae73276592e
Venca_X <Venca_X@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2009-05-13 17:06:35 +0000

    forgoten to commit mailboxSim ...

---
+++ src/remoteComp/remoteServerFilters/levelsetSegmentation/cellSrc/PPE/mailboxSimulator.h
@@ -0,0 +1,68 @@
+#ifndef MAILBOXSIMULATOR_H_
+#define MAILBOXSIMULATOR_H_
+
+#include <queue>
+#include "common/Thread.h"
+
+namespace M4D
+{
+namespace Cell
+{
+
+using namespace M4D::Multithreading;
+
+
+class MailboxSimulator
+{
+public:	
+	void PPEPush(uint32 mess)
+	{
+		ScopedLock lock(toSPEQMutex);
+		toSPEQueue.push(mess);
+		toSPEQCvar.notify_all();
+	}
+	uint32 PPEPop(void)
+	{
+		ScopedLock lock(fromSPEQMutex);
+		uint32 val = fromSPEQueue.front();
+		fromSPEQueue.pop();
+		return val;
+	}
+	
+	void SPEPush(uint32 mess)
+	{
+		fromSPEQueue.push(mess);
+	}
+	uint32 SPEPop(void)
+	{
+		ScopedLock lock(toSPEQMutex);
+		while(toSPEQueue.empty())
+			toSPEQCvar.wait(lock);
+		uint32 val = toSPEQueue.front();
+		toSPEQueue.pop();
+		return val;
+	}
+	
+	bool spe_queue_status()
+	{
+		ScopedLock lock(fromSPEQMutex);
+		return ! fromSPEQueue.empty();
+	}
+	
+	Mutex fromSPEQMutex;
+	Mutex toSPEQMutex;
+	
+private:
+	typedef std::queue<uint32> TMessageQueue;
+	
+	//#define MAX_QUEUE_LEN 4
+	TMessageQueue fromSPEQueue;
+	TMessageQueue toSPEQueue;
+
+	
+	CondVar toSPEQCvar;
+};
+
+}
+}
+#endif /*MAILBOXSIMULATOR_H_*/
