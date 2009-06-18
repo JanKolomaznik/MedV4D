fa5a730d32fa942474ba96eb237e63bae221c3de
Venca_X <Venca_X@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2009-06-18 06:07:40 +0000

    profiling defs

--- src/project/configurationSelector.mk
+++ src/project/configurationSelector.mk
@@ -6,6 +6,10 @@ CXXFLAGS += -Wall -Wno-deprecated -fstrict-aliasing -Wstrict-aliasing=2
 # verbose compiler
 #CXXFLAGS += -v
 
+# linker flags
+#-Wl,-q
+LDFLAGS=
+
 ####################### CONFs ############################
 ifeq "$(CONF)" "Debug"
 CXXFLAGS += -g -DDEBUG_LEVEL=10 -DDEBUG_ADITIONAL_INFO
@@ -15,24 +19,31 @@ ifeq "$(CONF)" "Release"
 CXXFLAGS += -O3
 endif
 
-ifeq "$(CONF)" "Profile"
-CXXFLAGS += -g -pg -DDEBUG_LEVEL=10 -DDEBUG_ADITIONAL_INFO
+ifeq "$(CONF)" "PDTProfile"
+#----------------- PDT profiling stuff ------------------
+INCLUDES += -I$(ROOT)/usr/include/trace
+LIBS += -L$(ROOT)/usr/lib64/trace
+CXXFLAGS += -g -DDEBUG_LEVEL=10 -DDEBUG_ADITIONAL_INFO
+LDFLAGS += -g -Wl,-q
+#--------------------------------------------------------
+endif
+
+ifeq "$(CONF)" "CPCProfile"
+#----------------- CPC profiling stuff ------------------
+CXXFLAGS += -g -DDEBUG_LEVEL=10 -DDEBUG_ADITIONAL_INFO
+LDFLAGS += -g -Wl,-q
+#--------------------------------------------------------
+endif
+
+ifeq "$(CONF)" "SPUTimingToolProfile"
+#----------- SPUTimingTool profiling stuff --------------
+# the same as Debug. Profiling is only for SPE
+# So appropriate defs are in SPE makefile
+CXXFLAGS += -g -DDEBUG_LEVEL=10 -DDEBUG_ADITIONAL_INFO -DSPU_TIMING_TOOL_PROFILING
+#--------------------------------------------------------
 endif
 ##########################################################
-
-
-# linker flags
-#-Wl,-q
-LD_PROFILE_OPTS= -pg
-
-ifeq "$(CONF)" "profile"
-LDFLAGS= $(LD_PROFILE_OPTS)
-endif
 
 #Dependecy file creation parameters
 DEP_FILE=	$(TMP_DIR)/depend
 CDEPFLAGS=	-MM
-
-ARFLAGS=	-r
-RM=rm -f
-MKDIR=mkdir
