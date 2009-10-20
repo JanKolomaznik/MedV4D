b769b2b07216687be2f31fc1531f52ce755d0764
ulman <ulman@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2009-10-20 18:18:47 +0000

    Creating new application: PerfusionStudies

---
+++ src/Applications/PerfusionStudies/mainWindow.cpp
@@ -0,0 +1,93 @@
+#include "mainWindow.h"
+#include "SettingsBox.h"
+#include "Imaging/PipelineMessages.h"
+
+using namespace std;
+using namespace M4D::Imaging;
+
+class LFNotifier : public M4D::Imaging::MessageReceiverInterface
+{
+public:
+	LFNotifier( AbstractPipeFilter * filter ): _filter( filter ) {}
+	void ReceiveMessage(M4D::Imaging::PipelineMessage::Ptr              msg, 
+                      M4D::Imaging::PipelineMessage::MessageSendStyle /*sendStyle*/, 
+                      M4D::Imaging::FlowDirection				              /*direction*/
+		)
+	{
+		if( msg->msgID == M4D::Imaging::PMI_FILTER_UPDATED ) {
+			_filter->ExecuteOnWhole();	
+		}
+	}
+protected:
+	AbstractPipeFilter * _filter;
+};
+
+void mainWindow::CreatePipeline()
+{
+	_convertor = new InImageConvertor();
+	_pipeline.AddFilter( _convertor );
+
+	_filter = new Thresholding();
+	_pipeline.AddFilter( _filter );
+
+	Median2D *medianFilter = new Median2D();
+
+	medianFilter->SetUpdateInvocationStyle( AbstractPipeFilter::UIS_ON_CHANGE_BEGIN );
+	medianFilter->SetRadius( 4 );
+	_pipeline.AddFilter( medianFilter );
+
+	MaskSelectionFilter *maskSelection = new MaskSelectionFilter();
+	_pipeline.AddFilter( maskSelection );
+
+	_inConnection = dynamic_cast<ConnectionInterfaceTyped<AbstractImage>*>( &_pipeline.MakeInputConnection( *_convertor, 0, false ) );
+	_pipeline.MakeConnection( *_convertor, 0, *_filter, 0 );
+	_tmpConnection = dynamic_cast<ConnectionInterfaceTyped<AbstractImage>*>( &_pipeline.MakeConnection( *_filter, 0, *medianFilter, 0 ) );
+	
+	_pipeline.MakeConnection( *_convertor, 0, *maskSelection, 0 );
+
+	ConnectionInterface* tmpStage2 = &(_pipeline.MakeConnection( *medianFilter, 0, *maskSelection, 1 ) );
+	tmpStage2->SetMessageHook( MessageReceiverInterface::Ptr( new LFNotifier( maskSelection ) ) );
+	_outConnection = dynamic_cast<ConnectionInterfaceTyped<AbstractImage>*>( &_pipeline.MakeOutputConnection( *maskSelection, 0, true ) );
+
+	if( _inConnection == NULL || _outConnection == NULL ) {
+		QMessageBox::critical( this, tr( "Exception" ), tr( "Pipeline error" ) );
+	}
+
+	addSource( _inConnection, "Segmentation", "Input" );
+	addSource( _tmpConnection, "Segmentation", "Stage #1" );
+	addSource( tmpStage2, "Segmentation", "Stage #2" );
+	addSource( _outConnection, "Segmentation", "Result" );
+
+  _notifier = new Notifier(this);
+	_outConnection->SetMessageHook( MessageReceiverInterface::Ptr( _notifier ) );
+}
+
+mainWindow::mainWindow ()
+  : m4dGUIMainWindow( APPLICATION_NAME, ORGANIZATION_NAME ), _inConnection( NULL ), _outConnection( NULL )
+{
+	Q_INIT_RESOURCE( mainWindow ); 
+
+	CreatePipeline();
+
+	_settings = new SettingsBox( _filter, this );
+	addDockWindow( "Bone Segmentation", _settings );
+	QObject::connect( _notifier, SIGNAL( Notification() ), _settings, SLOT( EndOfExecution() ), Qt::QueuedConnection );
+}
+
+void mainWindow::process ( AbstractDataSet::Ptr inputDataSet )
+{
+	try {
+		_inConnection->PutDataset( inputDataSet );
+
+		_convertor->Execute();
+
+		currentViewerDesktop->getSelectedViewerWidget()->InputPort()[0].UnPlug();
+		_inConnection->ConnectConsumer( currentViewerDesktop->getSelectedViewerWidget()->InputPort()[0] );
+
+		_settings->SetEnabledExecButton( true );
+	} 
+	catch( ... ) {
+		QMessageBox::critical( this, tr( "Exception" ), tr( "Some exception" ) );
+	}
+}
+
---
+++ src/Applications/PerfusionStudies/mainWindow.h
@@ -0,0 +1,77 @@
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#include "GUI/widgets/m4dGUIMainWindow.h"
+#include "Imaging/PipelineContainer.h"
+#include "Imaging/ImageFactory.h"
+#include "Imaging/filters/ThresholdingFilter.h"
+#include "Imaging/filters/MaskMedianFilter.h"
+#include "Imaging/filters/MaskSelection.h"
+#include "Imaging/filters/ImageConvertor.h"
+#include "SettingsBox.h"
+
+#define ORGANIZATION_NAME     "MFF"
+#define APPLICATION_NAME      "PerfusionStudies"
+
+typedef int16	ElementType;
+const unsigned Dim = 3;
+typedef M4D::Imaging::Image< ElementType, Dim > ImageType;
+typedef M4D::Imaging::ThresholdingMaskFilter< ImageType > Thresholding;
+typedef M4D::Imaging::MaskMedianFilter2D< Dim > Median2D;
+typedef M4D::Imaging::MaskSelection< ImageType > MaskSelectionFilter;
+typedef M4D::Imaging::ConnectionTyped< ImageType > InConnection;
+typedef M4D::Imaging::ImageConvertor< ImageType > InImageConvertor;
+
+class Notifier : public QObject, public M4D::Imaging::MessageReceiverInterface
+{
+	Q_OBJECT
+public:
+	Notifier( QWidget *owner ): _owner( owner ) {}
+	void ReceiveMessage(M4D::Imaging::PipelineMessage::Ptr 			        msg, 
+		                  M4D::Imaging::PipelineMessage::MessageSendStyle /*sendStyle*/, 
+		                  M4D::Imaging::FlowDirection				              /*direction*/
+		)
+	{
+		if( msg->msgID == M4D::Imaging::PMI_FILTER_UPDATED ) {
+			emit Notification();
+		}
+	}
+signals:
+	void Notification();
+protected:
+	QWidget	*_owner;
+};
+
+class mainWindow: public M4D::GUI::m4dGUIMainWindow
+{
+	Q_OBJECT
+
+public:
+
+	mainWindow ();
+
+protected:
+	void
+	process( M4D::Imaging::AbstractDataSet::Ptr inputDataSet );
+
+	void
+	CreatePipeline();
+
+	SettingsBox	*_settings;
+  Notifier * _notifier;
+
+	M4D::Imaging::PipelineContainer			_pipeline;
+	M4D::Imaging::AbstractPipeFilter		*_filter;
+	M4D::Imaging::AbstractPipeFilter		*_convertor;
+	M4D::Imaging::ConnectionInterfaceTyped< M4D::Imaging::AbstractImage >	*_inConnection;
+	M4D::Imaging::ConnectionInterfaceTyped< M4D::Imaging::AbstractImage >	*_tmpConnection;
+	M4D::Imaging::ConnectionInterfaceTyped< M4D::Imaging::AbstractImage >	*_outConnection;
+
+private:
+
+};
+
+
+#endif // MAIN_WINDOW_H
+
+
