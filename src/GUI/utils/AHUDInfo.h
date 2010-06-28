76a83bae41b3d405a75d9c988097e97b16393f97
johny <johny@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2010-06-28 15:07:03 +0000

    Continuing in design of new viewer interfaces

---
+++ src/GUI/utils/AHUDInfo.h
@@ -0,0 +1,29 @@
+#ifndef AHUD_INFO_H
+#define AHUD_INFO_H
+
+#include "common/Common.h"
+#include <list>
+
+
+
+namespace M4D
+{
+namespace GUI
+{
+namespace Viewer
+{
+
+class AHUDInfo
+{
+
+};
+
+
+typedef stl::list< AHUDInfo * >	HUDInfoList;
+
+} /*namespace Viewer*/
+} /*namespace GUI*/
+} /*namespace M4D*/
+
+
+#endif /*AHUD_INFO_H*/
--- src/GUI/widgets/AViewer.h
+++ src/GUI/widgets/AViewer.h
@@ -2,6 +2,7 @@
 #define AVIEWER_H
 
 #include "common/Common.h"
+#include "GUI/utils/AHUDInfo.h"
 
 namespace M4D
 {
@@ -10,6 +11,15 @@ namespace GUI
 namespace Viewer
 {
 
+
+enum RenderingQuality
+{
+	rqLowest,
+	rqLow,
+	rqNormal,
+	rqHigh,
+	rqHighest
+};
 
 
 class AViewer
@@ -23,11 +33,27 @@ public:
 
 	virtual ~AViewer() {}
 
+
+	/**
+	* Return the list of input ports connected to this viewer.
+	*  @return list of connected input ports
+	*/
+	const Imaging::InputPortList &
+	InputPort()const
+	{ return _inputPorts; }
+
+	/**
+	* List of the input ports connected to the given viewer.
+	*/
+	Imaging::InputPortList				_inputPorts;
+
 protected:
 
 	static int32	_lastID;
 
 	int32		_id;
+
+	HUDInfoList	_hudInfoList;
 private:
 	/**
 	 * Prohibition of copying.
