fae9d2ef16ac60815cd78ebf749b037631cd6601
johny <johny@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2010-07-19 15:23:58 +0000

    Continuing in viewer implementation

---
+++ src/GUI/widgets/A2DViewer.h
@@ -0,0 +1,28 @@
+#ifndef A_2D_VIEWER_H
+#define A_2D_VIEWER_H
+
+#include "GUI/widgets/AGUIViewer.h"
+#include <QtGui>
+#include <boost/shared_ptr.hpp>
+
+namespace M4D
+{
+namespace GUI
+{
+namespace Viewer
+{
+
+class A2DViewer: public AGUIViewer
+{
+
+};
+
+} /*namespace Viewer*/
+} /*namespace GUI*/
+} /*namespace M4D*/
+
+
+
+#endif /*A_2D_VIEWER_H*/
+
+
---
+++ src/GUI/widgets/A3DViewer.h
@@ -0,0 +1,29 @@
+#ifndef A_3D_VIEWER_H
+#define A_3D_VIEWER_H
+
+#include "GUI/widgets/AGUIViewer.h"
+#include <QtGui>
+#include <boost/shared_ptr.hpp>
+
+namespace M4D
+{
+namespace GUI
+{
+namespace Viewer
+{
+
+class A3DViewer: public AGUIViewer
+{
+
+};
+
+} /*namespace Viewer*/
+} /*namespace GUI*/
+} /*namespace M4D*/
+
+
+
+#endif /*A_3D_VIEWER_H*/
+
+
+
