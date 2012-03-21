586145bf27939d4585e9ce7e14739a1b3d8a9e81
johny <johny@3d4a5eb8-1416-4ba0-ad07-9ae941af739f>
2012-03-21 01:24:50 +0000

    Edge list construction somehow works

--- MedV4D/src/Imaging/cuda/RegionAdjacencyGraph.cu
+++ MedV4D/src/Imaging/cuda/RegionAdjacencyGraph.cu
@@ -21,7 +21,7 @@ struct VertexRecord
 struct EdgeRecord
 {
 	__device__  
-	EdgeRecord( uint32 aFirst, uint32 aSecond, float aWeight ): weight(aWeight), count( 1 )
+	EdgeRecord( uint32 aFirst, uint32 aSecond, float aWeight ): weight(aWeight)//, count( 1 )
 	{
 		if( aFirst < aSecond ) {
 			edgeCombIdx = (uint64(aFirst) << 32) + aSecond;
@@ -31,12 +31,12 @@ struct EdgeRecord
 		//assert( aFirst && aSecond && edgeCombIdx );
 	}
 	__device__  
-	EdgeRecord(): edgeCombIdx(0), weight(0.0f), count(0)
+	EdgeRecord(): edgeCombIdx(0), weight(0.0f)//, count(0)
 	{ }
 
 	uint64 edgeCombIdx;
 	float weight;
-	uint32 count;
+	//uint32 count;
 };
 
 struct GetSymmetricEdge : public thrust::unary_function< EdgeRecord, EdgeRecord >
@@ -68,6 +68,34 @@ struct IsValidEdge : public thrust::unary_function< EdgeRecord, EdgeRecord >
 	}
 };
 
+inline __device__ void
+hashEdge( EdgeRecord *aTable, size_t aSize, const EdgeRecord &aEdge )
+{
+	size_t idx = (0x2C87*(aEdge.edgeCombIdx % 0xC2FF97889)+0x159732CF) % aSize;
+	//size_t counter = 0;
+	//if( aEdge.edgeCombIdx == 0) 
+		//atomicAdd( &edgeInsertions, 1 );
+	bool inserted = false;
+	while ( !inserted ) {
+		if ( aTable[idx].edgeCombIdx == 0 ) {
+			if ( atomicCAS( &(aTable[idx].edgeCombIdx), uint64(0), aEdge.edgeCombIdx ) == 0 ) {
+				atomicAdd( &(aTable[idx].weight), aEdge.weight );
+				inserted = true;
+				return;
+			}
+		} else {
+			if ( aTable[idx].edgeCombIdx == aEdge.edgeCombIdx ) {
+				//atomicAdd( &(aTable[idx].count), aEdge.count );
+				atomicAdd( &(aTable[idx].weight), aEdge.weight );
+				inserted = true;
+				return;
+			}
+		}
+		idx = (idx+1) % aSize;
+	}
+}
+
+
 class EdgeHashTable: public Buffer1D<EdgeRecord>
 {
 public:
@@ -80,29 +108,7 @@ public:
 	__device__ void 
 	insertEdge( const EdgeRecord & aEdge )
 	{
-		assert( aEdge.edgeCombIdx );
-		size_t idx = aEdge.edgeCombIdx % mLength;
-		size_t counter = 0;
-		//if( /*aEdge.edgeCombIdx */idx == 0) 
-			atomicAdd( &edgeInsertions, 1 );
-		while ( true ) {
-			if ( mData[idx].edgeCombIdx == 0 ) {
-				if ( atomicCAS( &(mData[idx].edgeCombIdx), uint64(0), aEdge.edgeCombIdx ) == 0 ) {
-					atomicAdd( &(mData[idx].weight), aEdge.weight );
-					//if( mData[idx].edgeCombIdx == 0) atomicAdd( &edgeInsertions, 1 );
-					return;
-				}
-			} else {
-				if ( mData[idx].edgeCombIdx == aEdge.edgeCombIdx ) {
-					atomicAdd( &(mData[idx].count), aEdge.count );
-					atomicAdd( &(mData[idx].weight), aEdge.weight );
-					return;
-				}
-			}
-			idx = (idx+1) % mLength;
-			assert( ++counter < (mLength/10) );
-			if (counter > 1000) break;
-		}
+		hashEdge( mData, mLength, aEdge );
 	}
 };
 
@@ -110,7 +116,8 @@ template< typename TEType >
 __global__ void 
 preallocationOfAdjacencyGraph( Buffer3D< uint32 > aRegionBuffer, Buffer3D< TEType > aGradientBuffer, EdgeHashTable aEdgeHashMap, int3 blockResolution )
 {
-	__shared__ uint32 inData[MAX_SHARED_MEMORY];
+	__shared__ uint32 inData[10*10*10];
+	__shared__ EdgeRecord edges[ 3*8*8*8 ];
 	
 	const int cBlockDim = 8;
 	const int cRadius = 1;
@@ -130,32 +137,34 @@ preallocationOfAdjacencyGraph( Buffer3D< uint32 > aRegionBuffer, Buffer3D< TETyp
 	int idx2 = IdxFromCoordStrides( coordinates, aGradientBuffer.mStrides );
 
 	
-	//FillSharedMemory3D_8x8x8< uint32, cRadius, syStride, szStride >( inData, sidx, aRegionBuffer.mData, aRegionBuffer.mStrides, size, blockOrigin, coordinates, idx );
+	FillSharedMemory3D_8x8x8< uint32, cRadius, syStride, szStride >( inData, sidx, aRegionBuffer.mData, aRegionBuffer.mStrides, size, blockOrigin, coordinates, idx );
 	
 	__syncthreads();
 	if ( projected ) {
 		return;
 	}
-	uint32 current = aRegionBuffer.mData[idx];//inData[sidx];
-	//uint32 second = inData[sidx+1];
-	uint32 second = aRegionBuffer.mData[idx+1];
+	//uint32 current = aRegionBuffer.mData[idx];
+	uint32 current = inData[sidx];
+	uint32 second = inData[sidx+1];
+	//uint32 second = aRegionBuffer.mData[idx+1];
 	TEType val = aGradientBuffer.mData[idx2];
 	if( current == 0 ) return;
 	//assert( current );
+	
 	if ( current != second ) {
 		float weight = max( val, aGradientBuffer.mData[idx2+1] );
 		//assert( second );
 		if( second ) aEdgeHashMap.insertEdge( EdgeRecord( current, second, weight ) );
 	}
-	//second = inData[sidx+syStride];
-	second = aRegionBuffer.mData[idx+aRegionBuffer.mStrides.y];
+	second = inData[sidx+syStride];
+	//second = aRegionBuffer.mData[idx+aRegionBuffer.mStrides.y];
 	if ( current != second ) {
 		float weight = max( val, aGradientBuffer.mData[idx2+aGradientBuffer.mStrides.y] );
 		//assert( second );
 		if( second ) aEdgeHashMap.insertEdge( EdgeRecord( current, second, weight ) );
 	}
-	//second = inData[sidx+szStride];
-	second = aRegionBuffer.mData[idx+aRegionBuffer.mStrides.z];
+	second = inData[sidx+szStride];
+	//second = aRegionBuffer.mData[idx+aRegionBuffer.mStrides.z];
 	if ( current != second ) {
 		float weight = max( val, aGradientBuffer.mData[idx2+aGradientBuffer.mStrides.z] );
 		//assert( second );
@@ -191,12 +200,14 @@ computeRegionAdjacencyGraph( Buffer3D< uint32 > &aRegionBuffer, Buffer3D< TEType
 	D_PRINT( "edge insertions " << edgeInsertions );
 
 	CheckCudaErrorState( "After preallocationOfAdjacencyGraph()" );
-	size_t testedgeCount = thrust::count_if( aEdges.begin(), aEdges.end(), IsValidEdge() );
-	D_PRINT( "debug created " << testedgeCount << " edges" );
-	
+	//size_t testedgeCount = thrust::count_if( aEdges.begin(), aEdges.end(), IsValidEdge() );
+	//D_PRINT( "debug created " << testedgeCount << " edges" );
+	D_PRINT( "Time after preallocationOfAdjacencyGraph() " << clock.SecondsPassed() );
 
 	thrust::transform( aEdges.begin(), aEdges.begin() + aEdges.size()/2, aEdges.begin() + aEdges.size()/2, GetSymmetricEdge() );
+	D_PRINT( "Graph mirrored" );
 	thrust::sort( aEdges.begin(), aEdges.end(), CompareEdge() );
+	D_PRINT( "edges sorted" );
 	size_t edgeCount = thrust::count_if( aEdges.begin(), aEdges.end(), IsValidEdge() );
 	LOG( "computeRegionAdjacencyGraph computations took " << clock.SecondsPassed() );
 	D_PRINT( "created " << edgeCount << " edges" );
@@ -206,6 +217,7 @@ template< typename TEType >
 void
 createAdjacencyGraph( M4D::Imaging::ImageRegion< uint32, 3 > aLabeledMarkerRegions, M4D::Imaging::ImageRegion< TEType, 3 > aInput, size_t aRegionCount )
 {
+	D_PRINT( "Before " << __FUNCTION__ << ": " << cudaMemoryInfoText() );
 	Buffer3D< uint32 > labeledRegionsBuffer = CudaBuffer3DFromImageRegionCopy( aLabeledMarkerRegions );
 	aRegionCount = *(thrust::max_element( thrust::device_pointer_cast( labeledRegionsBuffer.mData ), thrust::device_pointer_cast( labeledRegionsBuffer.mData+labeledRegionsBuffer.mLength ) ));
 	D_PRINT( "Region count " << aRegionCount );
@@ -213,9 +225,10 @@ createAdjacencyGraph( M4D::Imaging::ImageRegion< uint32, 3 > aLabeledMarkerRegio
 	Buffer3D< TEType > inputBuffer = CudaBuffer3DFromImageRegionCopy( aInput );
 
 	thrust::device_vector< VertexRecord > vertices( 2/*aRegionCount*/ );
-	thrust::device_vector< EdgeRecord > edges( aRegionCount*10 );
-
+	thrust::device_vector< EdgeRecord > edges( aRegionCount*25 );
+	D_PRINT( "After allocation in " << __FUNCTION__ << ": " << cudaMemoryInfoText() );
 	computeRegionAdjacencyGraph( labeledRegionsBuffer, inputBuffer, edges, vertices );
+	D_PRINT( "After " << __FUNCTION__ << ": " << cudaMemoryInfoText() );
 }
 
 template void createAdjacencyGraph( M4D::Imaging::ImageRegion< uint32, 3 > aLabeledMarkerRegions, M4D::Imaging::ImageRegion< int8, 3 > aInput, size_t aRegionCount );
