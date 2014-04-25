1a668c966bf71dd1e5e56062645b645d9a7e5bb8
Jan Kolomaznik <honza.kolomaznik@gmail.com>
2014-04-25 18:14:43 +0200

    Different shader loading

--- MedV4D/include/config.h
+++ MedV4D/include/config.h
@@ -46,20 +46,30 @@
 /* Name of package */
 #define PACKAGE "tclap"
 
+#ifndef PACKAGE_BUGREPORT
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
+#endif //PACKAGE_BUGREPORT
 
+#ifndef PACKAGE_NAME
 /* Define to the full name of this package. */
 #define PACKAGE_NAME ""
+#endif //PACKAGE_NAME
 
+#ifndef PACKAGE_STRING
 /* Define to the full name and version of this package. */
 #define PACKAGE_STRING ""
+#endif //PACKAGE_STRING
 
+#ifndef PACKAGE_TARNAME
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME ""
+#	define PACKAGE_TARNAME ""
+#endif //PACKAGE_TARNAME
 
+#ifndef PACKAGE_VERSION
 /* Define to the version of this package. */
-#define PACKAGE_VERSION ""
+#	define PACKAGE_VERSION ""
+#endif //PACKAGE_VERSION
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
